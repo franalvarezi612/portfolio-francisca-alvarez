7a8a529854c79a4d91c442f3cab1406e83f94a67
franalvarezi612 <francisca.alvarezi@outlook.cl>
2025-06-21 00:33:32 +0200

    Agregar logo al componente NavBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,32 +1,53 @@
-import { View, Button } from 'react-native';
+import { View, Pressable, Text, StyleSheet, Image } from 'react-native';
 
 const NavBar = () => {
     return (
-        <View
-            style={styles.container}>
-            <Button
-            title='Inicio'
-            onPress={() => alert('Inicio presionado')}>
-            </Button>
-            <Button
-            title='Sobre mí'
-            onPress={() => alert('Sobre mí presionado')}>
-            </Button>
-            <Button
-            title='Proyectos'
-            onPress={() => alert('Proyectos presionado')}>
-            </Button>
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/icono-porfolio.png')}
+                style={styles.image}
+            />
+            <Pressable
+                style={[styles.button, { marginLeft: 'auto' }]}
+                onPress={() => alert('Inicio presionado')}>
+                <Text style={styles.buttonText}>Inicio</Text>
+            </Pressable>
+            <Pressable
+                style={styles.button}
+                onPress={() => alert('Sobre mí presionado')}>
+                <Text style={styles.buttonText}>Sobre mí</Text>
+            </Pressable>
+            <Pressable
+                style={styles.button}
+                onPress={() => alert('Proyectos presionado')}>
+                <Text style={styles.buttonText}>Proyectos</Text>
+            </Pressable>
         </View>
     )
-    }
+}
 
-const styles = {
+const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        justifyContent: 'flex-end   ',
         padding: 10,
         backgroundColor: '#fff',
+        alignItems: 'center', // Para una mejor alineación vertical
     },
-}
+    button: {
+        justifyContent: 'flex-end',
+        marginHorizontal: 5,
+        padding: 10,
+        backgroundColor: 'transparent',
+    },
+    buttonText: {
+        color: '#000000',
+        fontSize: 16,
+    },
+    image: {
+        width: 45,
+        height: 45,
+        resizeMode: 'contain', 
+    }
+});
 
 export default NavBar;
